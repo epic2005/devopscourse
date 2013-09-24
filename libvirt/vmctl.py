58988973dc0359348c7356ddd65b0f268b85fcdb
root <root@lipengyue.(none)>
2013-09-24 13:31:54 +0800

    use libvirt control vm demo.

--- libvirt/vmctl.py
+++ libvirt/vmctl.py
@@ -1,10 +1,50 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
+import sys
 import libvirt
 
-vconn = libvirt.open('qemu:///system')
-vconn.listAllDomains(0)
-freedos = vconn.lookupByName('freedos1')
+vconn = libvirt.open("qemu:///system")
+
+def listvm():
+    for vm in vconn.listAllDomains(0):
+        print "vm %s is %s" % (vm.name(), isrunning(vm))
+
+def getvm(n):
+    vm = vconn.lookupByName(n)
+    return vm
+
+def getvmstate(vm):
+    return vm.state(0)
+
+def startvm(vm):
+    ret = vm.create(vm)
+    return ret
+
+def isrunning(vm):
+    if vm.isActive() == 0:
+        ret = 'down'
+    elif vm.isActive() == 1:
+        ret = 'running'
+    else:
+        ret = 'status error.'
+    return ret
+
+def shutdownvm(vm):
+    ret = vm.destroy()
+    return ret
+
+def resetvm(vm):
+    ret = vm.reset(0)
+    return ret
+
+def descxml(vm):
+    xml = vm.XMLDesc(1)
+    return xml
 
 
-
+if __name__ == '__main__':
+    # list all vm
+    listvm()
+    # get vm xml.
+    vm = getvm('freedos')
+    print descxml(vm)
